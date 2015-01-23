4676b8382239081d24d7280e187f890a33b8f106
Oficina <copoliyo@gmail.com>
2015-01-23 13:06:36 +0100

    Formateo del texto. Ahora devuelve un objeto FacturaRecibida y se le añade un constructor para poder llamar a la clase sin necesidad de pasar una cuenta de Proveedor/Acreedor.

--- src/indices/IndiceFacturasRecibidas.java
+++ src/indices/IndiceFacturasRecibidas.java
@@ -7,6 +7,10 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -24,215 +28,300 @@ import util.Apariencia;
 import util.BaseDatos;
 import util.Cadena;
 import util.EscapeDialog;
-
-
+import util.Fecha;
 
 public class IndiceFacturasRecibidas {
-	public enum Columna {
-		FECHA(0), ORDEN(1), FACTURA(2), IMPORTE(3), IVA(4), TOTAL(5), FECHA_CONTABLE(6), ASIENTO_CONTABLE(7); 
 
-		private int value;
+    public enum Columna {
 
-		private Columna(int value) {
-			this.value = value;
-		}
-	}
-	
-	JDialog pantalla;
-	JTable jtFacturasRecibidas;
-	JScrollPane spFacturasRecibidas;
-	DefaultTableCellRenderer tcr;
-	TableCellRenderer tcr2;
-	
-	ResultSet rs = null;
-	MysqlConnect m = null;
-	
-	// Consulta SQL
-	String strSql = "";
-	String strCuenta = "";
+        FECHA(0), ORDEN(1), FACTURA(2), IMPORTE(3), IVA(4), TOTAL(5), FECHA_CONTABLE(6), ASIENTO_CONTABLE(7);
 
-	DefaultTableModel modeloTabla = new DefaultTableModel() {
-		@Override
-		public boolean isCellEditable(int row, int column) {
-			// Para que no podamos editar los datos de la tabla
-			return false;
-		}
-	};
-	
-	public IndiceFacturasRecibidas(String strCuenta){
-		this.strCuenta = strCuenta.trim();
-		m = MysqlConnect.getDbCon();
-		
-		creaGui();
-	}
-	
-	private void creaGui(){
-		pantalla = new EscapeDialog();
-		pantalla.setTitle("Facturas Recibidas");
-		pantalla.setModal(true);
-		pantalla.setSize(800, 600); 
-		pantalla.setDefaultCloseOperation(0);
-		pantalla.setLayout(null);
-		
-		// La tabla contendra estas columnas
-		modeloTabla.addColumn("Fecha");
-		modeloTabla.addColumn("Orden");
-		modeloTabla.addColumn("Factura");
-		modeloTabla.addColumn("Importe");
-		modeloTabla.addColumn("IVA");
-		modeloTabla.addColumn("Total");
-		modeloTabla.addColumn("Fecha Contable");
-		modeloTabla.addColumn("Asiento");
+        private int value;
 
-		jtFacturasRecibidas = new JTable(modeloTabla);
-		jtFacturasRecibidas.setFont(Apariencia.cambiaFuente(Font.PLAIN, 13));
-		
-		TableColumn columna = new TableColumn();
-		// Establecemos el ancho
-		jtFacturasRecibidas.getColumn("Fecha").setMaxWidth(80);
-		jtFacturasRecibidas.getColumn("Orden").setMaxWidth(60);
-		jtFacturasRecibidas.getColumn("Factura").setMaxWidth(100);
-		jtFacturasRecibidas.getColumn("Importe").setMaxWidth(120);
-		jtFacturasRecibidas.getColumn("IVA").setMaxWidth(120);
-		jtFacturasRecibidas.getColumn("Total").setMaxWidth(120);
-		jtFacturasRecibidas.getColumn("Fecha Contable").setMaxWidth(80);
-		jtFacturasRecibidas.getColumn("Asiento").setMaxWidth(80);
-		jtFacturasRecibidas.setRowHeight(20);
-		
-		
-		// Hacemos que las columnas se alineen a la DERECHA
-		tcr = new DefaultTableCellRenderer();
-		tcr.setHorizontalAlignment(SwingConstants.RIGHT);
-		jtFacturasRecibidas.getColumn("Fecha").setCellRenderer(tcr);
-		jtFacturasRecibidas.getColumn("Orden").setCellRenderer(tcr);
-		jtFacturasRecibidas.getColumn("Factura").setCellRenderer(tcr);
-		jtFacturasRecibidas.getColumn("Fecha Contable").setCellRenderer(tcr);
-		jtFacturasRecibidas.getColumn("Asiento").setCellRenderer(tcr);
-		
+        private Columna(int value) {
+            this.value = value;
+        }
+    }
+
+    JDialog pantalla;
+    JTable jtFacturasRecibidas;
+    JScrollPane spFacturasRecibidas;
+    DefaultTableCellRenderer tcr;
+    TableCellRenderer tcr2;
+
+    ResultSet rs = null;
+    MysqlConnect m = null;
+
+    // Consulta SQL
+    String strSql = "";
+    String strCuenta = "";
+
+    FacturaRecibida facturaRecibida = null;
+
+    DefaultTableModel modeloTabla = new DefaultTableModel() {
+        @Override
+        public boolean isCellEditable(int row, int column) {
+            // Para que no podamos editar los datos de la tabla
+            return false;
+        }
+    };
+
+    public IndiceFacturasRecibidas() {
+        this.strCuenta = "";
+        m = MysqlConnect.getDbCon();
+
+        creaGui();
+    }
+
+    public IndiceFacturasRecibidas(String strCuenta) {
+        this.strCuenta = strCuenta.trim();
+        m = MysqlConnect.getDbCon();
+
+        creaGui();
+    }
+
+    private void creaGui() {
+        pantalla = new EscapeDialog();
+        pantalla.setTitle("Facturas Recibidas");
+        pantalla.setModal(true);
+        pantalla.setSize(800, 600);
+        pantalla.setDefaultCloseOperation(0);
+        pantalla.setLayout(null);
+
+        // La tabla contendra estas columnas
+        modeloTabla.addColumn("Fecha");
+        modeloTabla.addColumn("Orden");
+        modeloTabla.addColumn("Factura");
+        modeloTabla.addColumn("Importe");
+        modeloTabla.addColumn("IVA");
+        modeloTabla.addColumn("Total");
+        modeloTabla.addColumn("Fecha Contable");
+        modeloTabla.addColumn("Asiento");
+
+        jtFacturasRecibidas = new JTable(modeloTabla);
+        jtFacturasRecibidas.setFont(Apariencia.cambiaFuente(Font.PLAIN, 13));
+
+        TableColumn columna = new TableColumn();
+        // Establecemos el ancho
+        jtFacturasRecibidas.getColumn("Fecha").setMaxWidth(80);
+        jtFacturasRecibidas.getColumn("Orden").setMaxWidth(60);
+        jtFacturasRecibidas.getColumn("Factura").setMaxWidth(100);
+        jtFacturasRecibidas.getColumn("Importe").setMaxWidth(120);
+        jtFacturasRecibidas.getColumn("IVA").setMaxWidth(120);
+        jtFacturasRecibidas.getColumn("Total").setMaxWidth(120);
+        jtFacturasRecibidas.getColumn("Fecha Contable").setMaxWidth(80);
+        jtFacturasRecibidas.getColumn("Asiento").setMaxWidth(80);
+        jtFacturasRecibidas.setRowHeight(20);
+
+        // Hacemos que las columnas se alineen a la DERECHA
+        tcr = new DefaultTableCellRenderer();
+        tcr.setHorizontalAlignment(SwingConstants.RIGHT);
+        jtFacturasRecibidas.getColumn("Fecha").setCellRenderer(tcr);
+        jtFacturasRecibidas.getColumn("Orden").setCellRenderer(tcr);
+        jtFacturasRecibidas.getColumn("Factura").setCellRenderer(tcr);
+        jtFacturasRecibidas.getColumn("Fecha Contable").setCellRenderer(tcr);
+        jtFacturasRecibidas.getColumn("Asiento").setCellRenderer(tcr);
+
 		// Hacemos que la comluna del saldo se alinee a la derecha y
-		// que salga en rojo si es negativa.
-		tcr2 = new TableCellRenderer();
-		jtFacturasRecibidas.getColumn("Importe").setCellRenderer(tcr2);
-		jtFacturasRecibidas.getColumn("IVA").setCellRenderer(tcr2);
-		jtFacturasRecibidas.getColumn("Total").setCellRenderer(tcr2);
-		
-		// Creamos un JscrollPane y le agregamos la JTable
-		spFacturasRecibidas = new JScrollPane(jtFacturasRecibidas);
-		// Si quisieramos barra horizontal, descomentar la linea siguiente
-		spFacturasRecibidas.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		// Agregamos el JScrollPane al contenedor
-		spFacturasRecibidas.setBounds(10, 10, 770, 560);	
-		spFacturasRecibidas.setFont(Apariencia.cambiaFuente());
-		pantalla.add(spFacturasRecibidas);			
-					
-		
-		cargaFacturasRecibidas();
-		
-		pantalla.setVisible(true);
-	}	
-	
-	class TableCellRenderer extends DefaultTableCellRenderer {
+        // que salga en rojo si es negativa.
+        tcr2 = new TableCellRenderer();
+        jtFacturasRecibidas.getColumn("Importe").setCellRenderer(tcr2);
+        jtFacturasRecibidas.getColumn("IVA").setCellRenderer(tcr2);
+        jtFacturasRecibidas.getColumn("Total").setCellRenderer(tcr2);
 
-		private static final long serialVersionUID = 1L;
+        // Creamos un JscrollPane y le agregamos la JTable
+        spFacturasRecibidas = new JScrollPane(jtFacturasRecibidas);
+        // Si quisieramos barra horizontal, descomentar la linea siguiente
+        spFacturasRecibidas.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        // Agregamos el JScrollPane al contenedor
+        spFacturasRecibidas.setBounds(10, 10, 770, 560);
+        spFacturasRecibidas.setFont(Apariencia.cambiaFuente());
 
-		public Component getTableCellRendererComponent(JTable table, Object value,
-				boolean isSelected, boolean hasFocus, int row, int col) {
+        jtFacturasRecibidas.addMouseListener(new TablaListener());
+        pantalla.add(spFacturasRecibidas);
 
-			Component c = super.getTableCellRendererComponent(table, value,
-					isSelected, hasFocus, row, col);
-			this.setHorizontalAlignment(SwingConstants.RIGHT);
-			Object valueAt = table.getModel().getValueAt(row, col);
-			String s = "";		      
+        cargaFacturasRecibidas();
 
-			if (valueAt != null) {
-				s = valueAt.toString();
-			}
+        pantalla.setVisible(true);
+    }
 
-			if (s.contains("-")) {
-				c.setForeground(Color.RED);
-				//c.setBackground(Color.gray);
-			} else {
-				c.setForeground(Color.black);
-				//c.setBackground(Color.WHITE);
-			}
+    class TableCellRenderer extends DefaultTableCellRenderer {
 
-			return c;
-		}
-	}
-	
-	public void cargaFacturasRecibidas(){
-		borrarTabla();
-						
-		Object fila[] = {"", "", "", "", "", "", "", ""};	
-		
-		String strSql = "SELECT * FROM FACREC WHERE EMPRESA = '" + DatosComunes.eEmpresa + "' AND " +
-				        " FACREC_CENTRO = " + DatosComunes.centroCont + " AND " +
-				        " FACREC_PROVACRE = '" + strCuenta + "' ORDER BY FACREC_FECHA ASC";				
-		String claveFechaAsientoApunte = "";
-		double baseTotal = 0.0;
-		
-		m = MysqlConnect.getDbCon();
-		
-		int numeroRegistros = BaseDatos.countRows(strSql);
-		if(numeroRegistros != 0){
-			String strFechaAsiento = "", strAsiento = "";
-			try {							
-				FacturaRecibida factura = new FacturaRecibida();
-				rs = m.query(strSql);				
-								
-				while(rs.next() || rs.isLast()){									
-					
-					factura.read(rs);
-					
-					claveFechaAsientoApunte = String.valueOf(factura.getFechaAsientoApunte());															
-					
-					strFechaAsiento = claveFechaAsientoApunte.substring(0, 8);
-					strAsiento = claveFechaAsientoApunte.substring(8, 13);
-					
-					fila[Columna.FECHA.value] = Cadena.fechaAcadena(Integer.valueOf(factura.getFecha()));
-					fila[Columna.ORDEN.value] = factura.getOrden();
-					fila[Columna.FACTURA.value] = factura.getFactura();
-					// Tenemos que sumar todas las bases
-					baseTotal = 0.0;
-					for(int i = 0; i < 4; i++)
-						baseTotal += factura.getBase(i);
-					
-					fila[Columna.IMPORTE.value] = Cadena.formatoConComaDecimal(baseTotal);
-					fila[Columna.IVA.value] = Cadena.formatoConComaDecimal(factura.getIva());
-					fila[Columna.TOTAL.value] = Cadena.formatoConComaDecimal(factura.getTotal());					
-					
-					fila[Columna.FECHA_CONTABLE.value] = Cadena.fechaAcadena(Integer.valueOf(strFechaAsiento));
-					fila[Columna.ASIENTO_CONTABLE.value] = Integer.valueOf(strAsiento);					
-					
-					modeloTabla.addRow(fila);
-					
-				}
-				rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				Apariencia.mensajeInformativo(5, "Error en lectura fichero de FacturaRecibida<BR>Consulta Acumulados Contables");				
-			}
+        private static final long serialVersionUID = 1L;
 
-			// Vamos al final de la tabla
-			int numeroDeFilas = modeloTabla.getRowCount() - 1;
-			jtFacturasRecibidas.clearSelection();
-			jtFacturasRecibidas.setRowSelectionInterval(numeroDeFilas, numeroDeFilas);
-			Rectangle rect = jtFacturasRecibidas.getCellRect(numeroDeFilas, 0, true);
-			jtFacturasRecibidas.scrollRectToVisible(rect);
-			jtFacturasRecibidas.clearSelection();
-			jtFacturasRecibidas.setRowSelectionInterval(numeroDeFilas, numeroDeFilas);
-			DefaultTableModel modelo = (DefaultTableModel)jtFacturasRecibidas.getModel();
-			   modelo.fireTableDataChanged();
-		}
-	}
-	
-	private void borrarTabla(){
+        public Component getTableCellRendererComponent(JTable table, Object value,
+                boolean isSelected, boolean hasFocus, int row, int col) {
+
+            Component c = super.getTableCellRendererComponent(table, value,
+                    isSelected, hasFocus, row, col);
+            this.setHorizontalAlignment(SwingConstants.RIGHT);
+            Object valueAt = table.getModel().getValueAt(row, col);
+            String s = "";
+
+            if (valueAt != null) {
+                s = valueAt.toString();
+            }
+
+            if (s.contains("-")) {
+                c.setForeground(Color.RED);
+                //c.setBackground(Color.gray);
+            } else {
+                c.setForeground(Color.black);
+                //c.setBackground(Color.WHITE);
+            }
+
+            return c;
+        }
+    }
+
+    public void cargaFacturasRecibidas() {
+        borrarTabla();
+
+        Object fila[] = {"", "", "", "", "", "", "", ""};
+
+        String strSql = "SELECT * FROM FACREC WHERE EMPRESA = '" + DatosComunes.eEmpresa + "' AND "
+                + " FACREC_CENTRO = " + DatosComunes.centroCont + " ";
+        
+        if(strCuenta.length() > 0)
+                strSql += " AND FACREC_PROVACRE = '" + strCuenta + "'";
+        
+        strSql += " ORDER BY FACREC_FECHA ASC";
+        
+        String claveFechaAsientoApunte = "";
+        double baseTotal = 0.0;
+
+        m = MysqlConnect.getDbCon();
+
+        int numeroRegistros = BaseDatos.countRows(strSql);
+        if (numeroRegistros != 0) {
+            String strFechaAsiento = "", strAsiento = "";
+            try {
+                FacturaRecibida factura = new FacturaRecibida();
+                rs = m.query(strSql);
+
+                while (rs.next() || rs.isLast()) {
+
+                    factura.read(rs);
+
+                    claveFechaAsientoApunte = String.valueOf(factura.getFechaAsientoApunte());
+
+                    strFechaAsiento = claveFechaAsientoApunte.substring(0, 8);
+                    strAsiento = claveFechaAsientoApunte.substring(8, 13);
+
+                    fila[Columna.FECHA.value] = Cadena.fechaAcadena(Integer.valueOf(factura.getFecha()));
+                    fila[Columna.ORDEN.value] = factura.getOrden();
+                    fila[Columna.FACTURA.value] = factura.getFactura();
+                    // Tenemos que sumar todas las bases
+                    baseTotal = 0.0;
+                    for (int i = 0; i < 4; i++) {
+                        baseTotal += factura.getBase(i);
+                    }
+
+                    fila[Columna.IMPORTE.value] = Cadena.formatoConComaDecimal(baseTotal);
+                    fila[Columna.IVA.value] = Cadena.formatoConComaDecimal(factura.getIva());
+                    fila[Columna.TOTAL.value] = Cadena.formatoConComaDecimal(factura.getTotal());
+
+                    fila[Columna.FECHA_CONTABLE.value] = Cadena.fechaAcadena(Integer.valueOf(strFechaAsiento));
+                    fila[Columna.ASIENTO_CONTABLE.value] = Integer.valueOf(strAsiento);
+
+                    modeloTabla.addRow(fila);
+
+                }
+                rs.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Apariencia.mensajeInformativo(5, "Error en lectura fichero de FacturaRecibida<BR>Consulta Acumulados Contables");
+            }
+
+            // Vamos al final de la tabla
+            int numeroDeFilas = modeloTabla.getRowCount() - 1;
+            jtFacturasRecibidas.clearSelection();
+            jtFacturasRecibidas.setRowSelectionInterval(numeroDeFilas, numeroDeFilas);
+            Rectangle rect = jtFacturasRecibidas.getCellRect(numeroDeFilas, 0, true);
+            jtFacturasRecibidas.scrollRectToVisible(rect);
+            jtFacturasRecibidas.clearSelection();
+            jtFacturasRecibidas.setRowSelectionInterval(numeroDeFilas, numeroDeFilas);
+            DefaultTableModel modelo = (DefaultTableModel) jtFacturasRecibidas.getModel();
+            modelo.fireTableDataChanged();
+        }
+    }
+
+    private void borrarTabla() {
 		// Vaciamos la tabla
-		//int a = modeloTabla.getRowCount() - 1;
+        //int a = modeloTabla.getRowCount() - 1;
 
 		//for (int i = a; i >= 0; i--)
-		//	modeloTabla.removeRow(i);
-		modeloTabla.setRowCount(0);
-	}
+        //	modeloTabla.removeRow(i);
+        modeloTabla.setRowCount(0);
+    }
+
+    class TablaListener implements ActionListener, MouseListener {
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            // TODO Auto-generated method stub
+            pantalla.setVisible(false);
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent arg0) {
+            // Si hacemos doble click fijamos el valor de la marca y salimos
+            if (arg0.getClickCount() == 2) {
+                facturaRecibida = new FacturaRecibida();
+                int fecha, orden;
+                String fechaStr;
+
+                JTable target = (JTable) arg0.getSource();
+                int row = target.getSelectedRow();
+                fechaStr = (String) target.getValueAt(row, Columna.FECHA.value);
+                fecha = Fecha.cadenaAfecha(fechaStr);
+                orden = (Integer) target.getValueAt(row, Columna.ORDEN.value);
+
+                String strSql = "SELECT * FROM FACREC WHERE EMPRESA = '" + DatosComunes.eEmpresa + "' AND "
+                        + " FACREC_CENTRO = " + DatosComunes.centroCont + " AND "
+                        + " FACREC_FECHA = " + fecha + " AND "
+                        + " FACREC_ORDEN = " + orden + " LIMIT 1";
+
+                if (facturaRecibida.read(strSql) == false) {
+                    facturaRecibida = null;
+                }
+
+                pantalla.setVisible(false);
+            }
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+    }
+    
+    public FacturaRecibida getFacturaRecibida(){
+            try {			
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.setVisible(false);
+		return facturaRecibida;
+            
+        }
+        
+    public void setVisible(boolean visible){
+        pantalla.setVisible(visible);
+    }
 }
