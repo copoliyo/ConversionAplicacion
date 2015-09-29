3c6f1f922658ec6e5c9b598a895cda88705c8cae
Oficina <copoliyo@gmail.com>
2015-09-29 13:43:10 +0200

    Cambios en eventos jbOk

--- src/procesos/EntradaAsientosContables.java
+++ src/procesos/EntradaAsientosContables.java
@@ -37,7 +37,12 @@ public class EntradaAsientosContables extends util.EscapeDialog implements Prope
     DefaultTableCellRenderer tcr;
     TableCellRenderer tcr2;
     
-    int asiento, fecha;
+    // Variables globales para cada asiento
+    int asiento = 0;
+    int fecha = 0;
+    boolean asientoNuevo = true;
+    int numeroApunte = 0;
+    int numeroTotalApuntes = 0;
 
     Vector<LineaMovimientoContable> vectorLineaMovimientos;
     
@@ -167,7 +172,9 @@ public class EntradaAsientosContables extends util.EscapeDialog implements Prope
         jbIvaAutomatico.setEnabled(false);
         jbMovimientos.setEnabled(false);
         jbPrevisiones.setEnabled(false);
-        
+        jbMasApuntes.setEnabled(false);
+        jbAnularApunte.setEnabled(false);
+        jbModificarApunte.setEnabled(false);
         
     }
     
@@ -238,6 +245,9 @@ public class EntradaAsientosContables extends util.EscapeDialog implements Prope
         jlNombreCuenta = new javax.swing.JLabel();
         jlTituloSaldo = new javax.swing.JLabel();
         jlSaldo = new javax.swing.JLabel();
+        jbMasApuntes = new javax.swing.JButton();
+        jbAnularApunte = new javax.swing.JButton();
+        jbModificarApunte = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Entrada de Asientos Contables");
@@ -261,6 +271,16 @@ public class EntradaAsientosContables extends util.EscapeDialog implements Prope
         });
 
         jbOkAsiento.setText("Ok");
+        jbOkAsiento.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbOkAsientoActionPerformed(evt);
+            }
+        });
+        jbOkAsiento.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jbOkAsientoKeyPressed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jpPanelFechaAsientoLayout = new javax.swing.GroupLayout(jpPanelFechaAsiento);
         jpPanelFechaAsiento.setLayout(jpPanelFechaAsientoLayout);
@@ -353,63 +373,81 @@ public class EntradaAsientosContables extends util.EscapeDialog implements Prope
 
         jlTituloSaldo.setText("Saldo:");
 
+        jbMasApuntes.setText("Más Apuntes");
+
+        jbAnularApunte.setText("Anular Apunte");
+
+        jbModificarApunte.setText("Modificar Apunte");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addComponent(jpPanelFechaAsiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(34, 34, 34)
-                .addComponent(jlNombreCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(26, 26, 26)
-                .addComponent(jlTituloSaldo)
-                .addGap(29, 29, 29)
-                .addComponent(jlSaldo, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(51, 51, 51)
-                .addComponent(jlCuadre)
-                .addGap(10, 10, 10)
-                .addComponent(jtfnf2DCuadre, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(34, 34, 34)
-                .addComponent(jlCuenta)
-                .addGap(108, 108, 108)
-                .addComponent(jlDocumento)
-                .addGap(10, 10, 10)
-                .addComponent(jlClave)
-                .addGap(43, 43, 43)
-                .addComponent(jlConcepto)
-                .addGap(340, 340, 340)
-                .addComponent(jlImporte))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(40, 40, 40)
-                .addComponent(jtfnfCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(6, 6, 6)
-                .addComponent(jbBuscarCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(6, 6, 6)
-                .addComponent(jtfnfDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(10, 10, 10)
-                .addComponent(jtfnfClave, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(2, 2, 2)
-                .addComponent(jbBuscarClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(6, 6, 6)
-                .addComponent(jtffConcepto, javax.swing.GroupLayout.PREFERRED_SIZE, 377, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(6, 6, 6)
-                .addComponent(jtfn2DImporte, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(6, 6, 6)
-                .addComponent(jbOkApunte))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addComponent(jbSalir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(137, 137, 137)
-                .addComponent(jbAnularAsiento, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
-                .addComponent(jbIvaAutomatico, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(74, 74, 74)
-                .addComponent(jbMovimientos, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(28, 28, 28)
-                .addComponent(jbPrevisiones, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jpPanelFechaAsiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(jlNombreCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(26, 26, 26)
+                        .addComponent(jlTituloSaldo)
+                        .addGap(29, 29, 29)
+                        .addComponent(jlSaldo, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(51, 51, 51)
+                        .addComponent(jlCuadre)
+                        .addGap(10, 10, 10)
+                        .addComponent(jtfnf2DCuadre, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(jlCuenta)
+                        .addGap(108, 108, 108)
+                        .addComponent(jlDocumento)
+                        .addGap(10, 10, 10)
+                        .addComponent(jlClave)
+                        .addGap(43, 43, 43)
+                        .addComponent(jlConcepto)
+                        .addGap(340, 340, 340)
+                        .addComponent(jlImporte))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jbSalir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jbAnularAsiento))
+                            .addComponent(jtfnfCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(6, 6, 6)
+                                .addComponent(jbBuscarCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(6, 6, 6)
+                                .addComponent(jtfnfDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(10, 10, 10)
+                                .addComponent(jtfnfClave, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(2, 2, 2)
+                                .addComponent(jbBuscarClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(6, 6, 6)
+                                .addComponent(jtffConcepto, javax.swing.GroupLayout.PREFERRED_SIZE, 377, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(6, 6, 6)
+                                .addComponent(jtfn2DImporte, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(6, 6, 6)
+                                .addComponent(jbOkApunte))
+                            .addGroup(layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jbIvaAutomatico)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jbMasApuntes)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jbAnularApunte)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jbModificarApunte)
+                                .addGap(18, 18, 18)
+                                .addComponent(jbMovimientos)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jbPrevisiones, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addGap(54, 54, 54))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -449,10 +487,14 @@ public class EntradaAsientosContables extends util.EscapeDialog implements Prope
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jbSalir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbAnularAsiento)
-                    .addComponent(jbIvaAutomatico)
-                    .addComponent(jbMovimientos)
-                    .addComponent(jbPrevisiones)))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jbAnularAsiento)
+                        .addComponent(jbIvaAutomatico)
+                        .addComponent(jbMasApuntes)
+                        .addComponent(jbAnularApunte)
+                        .addComponent(jbModificarApunte)
+                        .addComponent(jbMovimientos)
+                        .addComponent(jbPrevisiones))))
         );
 
         pack();
@@ -491,6 +533,70 @@ public class EntradaAsientosContables extends util.EscapeDialog implements Prope
         }
         
     }//GEN-LAST:event_jtfnfAsientoActionPerformed
+
+    private void jbOkAsientoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbOkAsientoActionPerformed
+        fecha = jtffeFechaAsiento.getFechaAAAAMMDD();
+                
+        if(jtfnfAsiento.getText().trim().length() == 0)
+            jtfnfAsiento.setText("0");
+        
+        asiento = Integer.valueOf(jtfnfAsiento.getText().trim());
+
+        // Si el campo de asiento está vacío o no existe el asiento que pretendemos visualizar, busca el primero libre.
+        if (asiento == 0 || util.MovimientosContables.existeMovimiento(DatosComunes.centroCont, fecha, asiento) == false) {
+            // Ponemos el número del primer asiento libre
+            jtfnfAsiento.setText(String.valueOf(util.MovimientosContables.buscaPrimeroLibreEnDia(DatosComunes.centroCont, fecha)));
+            // Borramos las lineas del asiento, está vacío
+            modeloTabla.setRowCount(0);
+            
+        } else {
+            if (util.MovimientosContables.existeMovimiento(DatosComunes.centroCont, fecha, asiento) == true) {
+                vectorLineaMovimientos = MovimientosContables.leeAsiento(DatosComunes.centroCont, fecha, asiento);
+                displayLineasAsiento();
+                System.out.println("Apuntes en el asiento: " + vectorLineaMovimientos.size());
+            }
+        }
+        
+        if(fecha <= DatosComunes.fecUltCierre || fecha < DatosComunes.fecUltRegpro){
+           util.Apariencia.mensajeInformativo(9, "Fecha no permitida!!!");
+        }
+        
+        if(fecha == DatosComunes.fecUltRegpro && asiento != 99997){
+           util.Apariencia.mensajeInformativo(9, "Fecha no permitida!!!"); 
+        }
+    }//GEN-LAST:event_jbOkAsientoActionPerformed
+
+    private void jbOkAsientoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jbOkAsientoKeyPressed
+        fecha = jtffeFechaAsiento.getFechaAAAAMMDD();
+                
+        if(jtfnfAsiento.getText().trim().length() == 0)
+            jtfnfAsiento.setText("0");
+        
+        asiento = Integer.valueOf(jtfnfAsiento.getText().trim());
+
+        // Si el campo de asiento está vacío o no existe el asiento que pretendemos visualizar, busca el primero libre.
+        if (asiento == 0 || util.MovimientosContables.existeMovimiento(DatosComunes.centroCont, fecha, asiento) == false) {
+            // Ponemos el número del primer asiento libre
+            jtfnfAsiento.setText(String.valueOf(util.MovimientosContables.buscaPrimeroLibreEnDia(DatosComunes.centroCont, fecha)));
+            // Borramos las lineas del asiento, está vacío
+            modeloTabla.setRowCount(0);
+            
+        } else {
+            if (util.MovimientosContables.existeMovimiento(DatosComunes.centroCont, fecha, asiento) == true) {
+                vectorLineaMovimientos = MovimientosContables.leeAsiento(DatosComunes.centroCont, fecha, asiento);
+                displayLineasAsiento();
+                System.out.println("Apuntes en el asiento: " + vectorLineaMovimientos.size());
+            }
+        }
+        
+        if(fecha <= DatosComunes.fecUltCierre || fecha < DatosComunes.fecUltRegpro){
+           util.Apariencia.mensajeInformativo(9, "Fecha no permitida!!!");
+        }
+        
+        if(fecha == DatosComunes.fecUltRegpro && asiento != 99997){
+           util.Apariencia.mensajeInformativo(9, "Fecha no permitida!!!"); 
+        }
+    }//GEN-LAST:event_jbOkAsientoKeyPressed
     
     private void displayLineasAsiento(){
         
@@ -531,11 +637,14 @@ public class EntradaAsientosContables extends util.EscapeDialog implements Prope
     private javax.swing.JButton jbPrueba;
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jbAnularApunte;
     private javax.swing.JButton jbAnularAsiento;
     private javax.swing.JButton jbBuscarAsiento;
     private javax.swing.JButton jbBuscarClave;
     private javax.swing.JButton jbBuscarCuenta;
     private javax.swing.JButton jbIvaAutomatico;
+    private javax.swing.JButton jbMasApuntes;
+    private javax.swing.JButton jbModificarApunte;
     private javax.swing.JButton jbMovimientos;
     private javax.swing.JButton jbOkApunte;
     private javax.swing.JButton jbOkAsiento;
